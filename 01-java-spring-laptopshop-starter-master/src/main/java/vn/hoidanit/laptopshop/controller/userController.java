ba2d6220c51b81f576420b046cda05ede0d3d5fb
TruongDay2004 <gatrex5qln@gmail.com>
2026-09-01 17:09:40 +0700

    learn jsp , html

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/userController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/userController.java
@@ -22,7 +22,7 @@ public class userController {
     @RequestMapping("/")
     public String getHomePage() {
         String test = this.userService.handleHello();
-        return "eric.html";
+        return "hello";
     }
 
 }
